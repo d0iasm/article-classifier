42972d6311edc7d9fc084ff212bb43ff971746d6
Asami Doi <doiasami1219@gmail.com>
2017-03-21 05:57:04 +0900

    finish morphological analysis library

--- naivebayes/lib/morpheme.py
+++ naivebayes/lib/morpheme.py
@@ -1,11 +1,16 @@
 from janome.tokenizer import Tokenizer
 
 class Morpheme:
-    def analysis(self):
+    def analysis(self, text):
+        noun_list = []
         t = Tokenizer()
-        for token in t.tokenize(u'すもももももももものうち'):
-            print(token)
+        self.text = text
+        for token in t.tokenize(self.text):
+            part_of_speech = token.part_of_speech.split(',')[0]
+            if part_of_speech == u'名詞':
+                noun_list.append(token.surface)
+        return noun_list
 
 if __name__ == '__main__':
     morpheme = Morpheme()
-    morpheme.analysis()
+    print(morpheme.analysis('我輩は猫である。名前はまだ無い。'))
