31c6ae2d9ddc0cc984efc4bda25f992ee71d6e05
Asami Doi <doiasami1219@gmail.com>
2017-03-21 21:31:03 +0900

    delete unused import

--- naivebayes/views.py
+++ naivebayes/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import requests
 
 from .lib.forms import Forms
+from .lib.naivebayes import *
 
 
 def index(request):
@@ -19,7 +20,26 @@ def index(request):
     return render(request, 'naivebayes/index.html', context)
 
 def learn(request):
+    training_data = [["good", [u"よい", u"とても"]],
+                   ["good", [u"よい", u"とても", u"すばらしい"]],
+                   ["good", [u"よい", u"すばらしい", u"見つかりません"]],
+                   ["good", [u"すばらしい"]],
+                   ["bad",  [u"見つかりません", u"買いたくない"]],
+                   ["bad",  [u"よい"]],
+                   ["bad",  [u"買いたくない", u"最悪"]],
+                   ["bad",  [u"最悪"]]]
+
+    # テスト用データ
+    test_data  = [u"よい", u"とても"]
+
+    classifier = NaiveBayesClassifier()
+
+    # 学習フェーズ
+    for c, f in training_data:
+      classifier.learn(c, f)
+
     context = {
-        'hoge': 'hoge',
+        'test': classifier.classifly(test_data)
     }
+    
     return render(request, 'naivebayes/learn.html', context)
