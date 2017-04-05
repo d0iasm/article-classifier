f73a1c6ce9394640c6f2adea592a17b2a910d135
Asami Doi <doiasami1219@gmail.com>
2017-04-05 14:52:03 +0900

    add: models to admin site

--- naivebayes/lib/training/training.py
+++ naivebayes/lib/training/training.py
@@ -13,11 +13,11 @@ class Training:
 
     def training(self):
         morpheme = Morpheme()
-        for category in data.training_data:
-            res = requests.get(category[1])
+        for data in data.training_data:
+            res = requests.get(data[1])
             # res = requests.get(form.cleaned_data['url'])
             japanese_text = morpheme.filter(res.text)
             noun_list = morpheme.analysis(japanese_text)
             # category = form.cleaned_data['category']
             classifier = NaiveBayesClassifier()
-            classifier.learn(category[0], noun_list)
+            classifier.learn(data[0], noun_list)
